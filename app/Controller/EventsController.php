7672bb8597065ac8b622fee0bd55b42f0e73eec8
anthony0629 <anthony0629@gmail.com>
2014-11-23 11:53:01 -0500

    Event, EventsController, and Events view folder added

---
+++ app/Controller/EventsController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anthony
+ * Date: 11/23/2014
+ * Time: 11:43 AM
+ */
+
+class EventsController extends AppController {
+
+} 
---
+++ app/Model/Event.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anthony
+ * Date: 11/23/2014
+ * Time: 11:41 AM
+ */
+
+class Event extends AppModel {
+
+} 
